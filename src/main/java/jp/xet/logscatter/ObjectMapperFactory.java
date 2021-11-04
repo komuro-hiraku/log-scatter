80ce65e89dad1ca59e944cdb5daa6afb8a2a82b2
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-11-04 20:01:20 +0900

    fix comment

--- src/main/java/jp/xet/logscatter/ObjectMapperFactory.java
+++ src/main/java/jp/xet/logscatter/ObjectMapperFactory.java
@@ -8,12 +8,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ObjectMapperFactory {
 
-    /**
-     * Mantaで使用するObjectMapperを生成する。<br>
-     * <p>実装はBeanとして利用し、主にTest側で共通設定で利用するため</p>
-     *
-     * @return {@link ObjectMapper}
-     */
     public static ObjectMapper createObjectMapper() {
         return new ObjectMapper()
                 .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
